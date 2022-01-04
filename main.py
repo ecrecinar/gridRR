bb6d0a313107d65ff28d8cd00d665c72bc75e017
Ecre <ecrecinar@gmail.com>
2022-01-04 17:09:04 +0300

    Co-authored-by: borasanuk <borasanuk@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import shapely.geometry
 import pyproj
 import random
 import numpy as np
+import json
 
 PROB = 0.82
 STEPSIZE = 100
@@ -40,8 +41,8 @@ def create_grid(step_size, lat, lng):
                       ((int)(transformed_loc[1] / step_size)) * step_size - step_size)
     transformed_ne = transformed_sw[0] + \
         (3 * step_size), transformed_sw[1] + (3 * step_size)
-    print(transformed_sw)
-    print(transformed_ne)
+    # print(transformed_sw)
+    # print(transformed_ne)
     grid_points = []
     cells = [[] for _ in range(9)]
     x = transformed_sw[0]
@@ -58,19 +59,28 @@ def create_grid(step_size, lat, lng):
     Y = []
 
     for i in range(len(grid_points)):
-        print(grid_points[i].x, ",", grid_points[i].y)
+        # print(grid_points[i].x, ",", grid_points[i].y)
         X.append(grid_points[i].x)
         Y.append(grid_points[i].y)
 
-    i = 1
-    plt.close()
-    plt.scatter(Y, X)
-    plt.ticklabel_format(useOffset=False)
-    plt.scatter([Y[i], Y[i+1], Y[i+4], Y[i+5]], [X[i], X[i+1], X[i+4], X[i+5]])
-    plt.show()
+    # i = 1
+    # plt.close()
+    # plt.scatter(Y, X)
+    # plt.ticklabel_format(useOffset=False)
+    # plt.scatter([Y[i], Y[i+1], Y[i+4], Y[i+5]], [X[i], X[i+1], X[i+4], X[i+5]])
+    # plt.show()
+
+    cells = []
+    for i in range(11):
+        if i == 3 or i == 7:
+            continue
+        cells.append([(Y[i],X[i]), (Y[i+1],X[i+1]), (Y[i+4],X[i+4]),(Y[i+5],X[i+5])])
+
+    return cells
+    # print(cells)
 
 
-def RR(cells, probability):
+def RR(probability):
     """[summary]
 
     Args:
@@ -80,19 +90,70 @@ def RR(cells, probability):
     Returns:
         Square:
     """
-    if(random.random(0, 1) < probability):
-        return cells[5]
+    if(random.random() < probability):
+        return 5
     else:
-        cells[5] = 0
-        cells.remove(0)
-        return np.random.choice(cells)
+        return np.random.choice(range(0,9),1,[0.125,0.125,0.125,0.125,0,0.125,0.125,0.125,0.125])[0]
 
 
-# def (step_size, lat, lng, probability):
-#    cells = create_grid(step_size, lat, lng)
-#    return RR(cells, probability)
+def get_trajectory(file):
+    locations = dict()
+    trajectory = []
+    lines = []
+    with open(file) as f:
+        lines = f.readlines()
+    
+    for line in lines:
+        current_loc = [x.strip() for x in line.split(',')]
+        cells = create_grid(STEPSIZE, float(current_loc[0]), float(current_loc[1]))
+        index = RR(PROB)
+        trajectory.append(cells[index])
+        if str(cells[index]) in locations:
+            locations[str(cells[index])] = locations[str(cells[index])] + 1
+        else:
+            locations[str(cells[index])] = 1
+    return trajectory
+
+user1_trajectory =  get_trajectory('user1_data.txt')   
+# print(user1_trajectory)
+
+user2_trajectory =  get_trajectory('user2_data.txt')   
+# print(user2_trajectory)
+
+user3_trajectory =  get_trajectory('user3_data.txt')   
+# print(user3_trajectory)
+
+max_entry = max(len(user1_trajectory), len(user2_trajectory), len(user3_trajectory))
+print(len(user1_trajectory))
+print(len(user2_trajectory))
+print(len(user3_trajectory))
+
+all_grids = user1_trajectory
+all_grids.extend(user2_trajectory)
+all_grids.extend(user3_trajectory)
+print(all_grids)
+print(len(all_grids))
+
+# time_grid_data = {}
+# counts = {}
+
+# for i in range(len(all_grids)):
+#     counts[str(all_grids[i])] = 0
+
+# for i in range(max_entry):
+#     time_grid_data[i] = counts
 
 
-# LDP_grid = gridRR(STEPSIZE, LAT, LNG, PROB)
+# for i in range(max_entry):
+#     print(time_grid_data[i][str(user1_trajectory[i])])
+#     print(time_grid_data[i][str(user2_trajectory[i])])
+#     if i < len(user1_trajectory):
+#         time_grid_data[i][str(user1_trajectory[i])] = time_grid_data[i][str(user1_trajectory[i])] + 1
+#     if i < len(user2_trajectory):
+#         time_grid_data[i][str(user2_trajectory[i])] = time_grid_data[i][str(user2_trajectory[i])] + 1
+#     if i < len(user3_trajectory):
+#         time_grid_data[i][str(user3_trajectory[i])] = time_grid_data[i][str(user3_trajectory[i])] + 1
 
-create_grid(100, 10.0, 0.0)
+# # print(time_grid_data)
+# with open("test.json", "w") as outfile:
+#     json.dump(time_grid_data, outfile)
