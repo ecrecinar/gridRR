fbba1039c1aa68f1f7120a373674d102e4117a79
Michael Bora Sanuk <bora@aeontech.io>
2022-01-17 22:34:34 +0300

    added timestamps to dataset

--- grid_rr.py
+++ grid_rr.py
@@ -3,6 +3,7 @@ from utils import colors
 from visualization import *
 from matplotlib.ticker import NullFormatter, FixedLocator
 import data_processing as dp
+from datetime import datetime
 import os
 
 DEFAULT_PROB = 0.82
@@ -11,7 +12,7 @@ DEFAULT_STEPSIZE = 20
 
 def run(step_size=DEFAULT_STEPSIZE, rr_prob=DEFAULT_PROB, show_result=False):
     data = dp.get_trajectories()
-    current_time = datetime.now().strftime("%H-%M-%S--%d-%m-%Y")
+    current_time = datetime.now().strftime("%H-%M-%S-%d-%m-%Y")
     directory = "experiments/experiment-" + current_time
     os.makedirs(directory)
     gts = []
